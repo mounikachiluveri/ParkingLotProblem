276d08c374e3df54965ebadc3d49329aff3d8a76
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-10 22:27:27 +0530

    Added :Test case when the capacity Exceded should throw exception

--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -30,4 +30,15 @@ public class ParkingLotTest {
         boolean isRemoved = parkingLot.vehicleUnparking("333");
         Assert.assertFalse(isRemoved);
     }
+
+    @Test
+    public void givenVehicles_WhenCapacityExceeds_ShouldThrowException() {
+        try {
+            ParkingLotSystem parkingLot = new ParkingLotSystem();
+            String[] vehicleNumber = {"111", "222", "333"};
+            parkingLot.vehicleParking(vehicleNumber);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals(e.type, ParkingLotException.ExceptionType.CAPACITY_EXCEEDED);
+        }
+    }
 }
