9fa4de284920c556c30299f114c65db38816d11d
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-10 20:13:35 +0530

    Added:Test case for Parking the Vehicle

---
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -0,0 +1,13 @@
+package parkinglot;
+
+public class ParkingLotSystem {
+    public ParkingLotSystem(Object vehicle) {
+    }
+
+    public ParkingLotSystem() {
+    }
+
+    public Boolean park(Object vehicle) {
+        return true;
+    }
+}
